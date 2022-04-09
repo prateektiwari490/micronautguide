8b0a29dec14caa52ba147ea5ebc349d1a2306e19
Prateek Tiwari <prateektiwari490@gmail.com>
2022-04-09 13:59:29 +0530

    HttpClient header and filtering technique

---
+++ src/main/kotlin/example/micronaut/GithubFilter.kt
@@ -0,0 +1,18 @@
+package example.micronaut
+
+import io.micronaut.context.annotation.Requires
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MutableHttpRequest
+import io.micronaut.http.annotation.Filter
+import io.micronaut.http.filter.ClientFilterChain
+import io.micronaut.http.filter.HttpClientFilter
+import org.reactivestreams.Publisher
+
+@Filter("/repos/**")
+@Requires(condition = GithubFilterCondition::class)
+class GithubFilter(val configuration: GithubConfiguration) : HttpClientFilter {
+
+    override fun doFilter(request: MutableHttpRequest<*>, chain: ClientFilterChain): Publisher<out HttpResponse<*>?> {
+        return chain.proceed(request.basicAuth(configuration.username, configuration.token))
+    }
+}
---
+++ src/main/kotlin/example/micronaut/GithubFilterCondition.kt
@@ -0,0 +1,24 @@
+package example.micronaut
+
+import io.micronaut.context.condition.Condition
+import io.micronaut.context.condition.ConditionContext
+import io.micronaut.context.exceptions.NoSuchBeanException
+
+class GithubFilterCondition : Condition {
+
+    override fun matches(context: ConditionContext<*>?): Boolean {
+        if (context != null && context.beanContext != null) {
+            try {
+                val githubConfiguration: GithubConfiguration =
+                    context.beanContext.getBean(GithubConfiguration::class.java)
+                println("githubConfiguration.token : ${githubConfiguration.token}")
+                if (githubConfiguration.token != null && githubConfiguration.username != null) {
+                    return true
+                }
+            } catch (e: NoSuchBeanException) {
+            }
+        }
+        return false
+    }
+
+}
